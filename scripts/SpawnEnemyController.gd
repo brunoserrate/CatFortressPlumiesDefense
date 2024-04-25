48a4429ec2927e5283a433c691fb4daee4be4eca
brunoserrate <brunostj@hotmail.com>
2024-04-24 22:42:35 -0300

    #5 - feat: add health bar to the enemy base (enemy crystal)

--- scripts/SpawnEnemyController.gd
+++ scripts/SpawnEnemyController.gd
@@ -1,3 +1,22 @@
 extends Node2D
 
 class_name SpawnEnemyController
+
+export var health = 100
+
+# Hp Bar Texture Progress Bar
+var hpBar: TextureProgress
+
+# TODO: Add a way to set the enemy type
+
+func _ready():
+	hpBar = $CrystalHpBar
+	hpBar.max_value = health
+	hpBar.value = health
+
+func receive_damage(damage):
+	health -= damage
+	hpBar.value -= damage
+
+	if health <= 0:
+		queue_free()
