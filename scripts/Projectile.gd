6a1a0ec99bb02a7f25f97501bbccd7262f078b3e
brunoserrate <brunostj@hotmail.com>
2024-05-28 00:01:22 -0300

    feat: added projectiles assets and scripts;feat: added ranged attack functionality

---
+++ scripts/Projectile.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+export(float) var speed = 100
+export(String) var group_name
+
+var velocity = Vector2.ZERO
+var target = null
+var damage = 0
+
+var group_list = []
+
+func _ready():
+	add_to_group(group_name)
+
+func _physics_process(delta):
+	if (velocity == Vector2.ZERO):
+		return
+
+	var collisions = move_and_collide(velocity * delta)
+
+	if !collisions:
+		return
+
+	for group in group_list:
+		if collisions.collider.is_in_group(group):
+			collisions.collider.receive_damage(damage)
+			queue_free()
+
+func set_target(_target):
+	target = _target
+
+func set_damage(_damage):
+	damage = _damage
+
+func set_group_list(_group_list):
+	group_list = _group_list
+
+func set_velocity():
+	if is_instance_valid(target) and velocity == Vector2.ZERO:  # Só define se a velocidade ainda não foi definida
+		velocity = (target.global_position - global_position).normalized() * speed
--- scripts/Unit.gd
+++ scripts/Unit.gd
@@ -19,6 +19,9 @@ export(Vector2) var default_direction = Vector2.RIGHT
 # group list to check
 export var group_list = ["EnemyUnit", "EnemySpawner", "AlliedUnit", "AlliedSpawner"]
 
+export(PackedScene) var projectile
+export(Vector2) var projectile_offset = Vector2(40, 20)
+
 onready var health = max_health
 onready var state = GlobalEnums.UnitState.IDLE
 onready var health_bar = $HealthBar
@@ -41,43 +44,39 @@ func _process(delta):
 	if(attack_animation_is_playing()):
 		return
 
-	if(state == GlobalEnums.UnitState.ATTACKING):
-		attack()
-
-
-func _physics_process(delta):
-	if(attack_animation_is_playing()):
-		return
-
-	if(state == GlobalEnums.UnitState.MOVING):
-		move_unit(delta)
+	match state:
+		GlobalEnums.UnitState.IDLE:
+			pass
+		GlobalEnums.UnitState.MOVING:
+			move_unit(delta)
+		GlobalEnums.UnitState.ATTACKING:
+			attack(delta)
 
 func _on_AttackArea_area_entered(area):
 	var parent = area.get_parent()
 
-	if(attack_type == GlobalEnums.UnitAttackType.RANGED):
-		for group in group_list:
-			if(parent.is_in_group(group)):
-				target = parent
-				target_list.append(parent)
-
-				clear_invalid_targets()
-				set_nearest_target()
-				state = GlobalEnums.UnitState.ATTACKING
-				set_velocity(Vector2.ZERO)
-				return
-	elif(attack_type == GlobalEnums.UnitAttackType.MELEE):
-		for group in group_list:
-			if(parent.is_in_group(group)):
-				if(target_list.size() > 0):
+	if state == GlobalEnums.UnitState.IDLE or state == GlobalEnums.UnitState.MOVING:
+		if attack_type == GlobalEnums.UnitAttackType.RANGED:
+			for group in group_list:
+				if parent.is_in_group(group):
+					target = parent
 					target_list.append(parent)
 					clear_invalid_targets()
-
-				set_target_position(parent)
+					set_nearest_target()
+					state = GlobalEnums.UnitState.ATTACKING
+					set_velocity(Vector2.ZERO)
+					return
+		elif attack_type == GlobalEnums.UnitAttackType.MELEE:
+			for group in group_list:
+				if parent.is_in_group(group):
+					if target_list.size() > 0:
+						target_list.append(parent)
+						clear_invalid_targets()
+					set_target_position(parent)
+					return
 
 # Internal functions
 func move_unit(delta):
-
 	if(animated_sprite.animation != "run"):
 		animated_sprite.play("run")
 
@@ -115,27 +114,55 @@ func die():
 	yield(get_tree().create_timer(delay_to_die), "timeout")
 	queue_free()
 
-func attack():
+func attack(delta):
+	if(!is_instance_valid(self)):
+		return
+
 	if(!is_instance_valid(target) || target == null):
 		state = GlobalEnums.UnitState.MOVING
 		return
 
-	attack_timer += get_process_delta_time()
+	if(attack_animation_is_playing()):
+		return
+
+	attack_timer += delta
 
 	if(attack_timer < attack_rate):
 		return
 
+	attack_timer = 0
+
+	state = GlobalEnums.UnitState.ATTACKING
+
 	if(animated_sprite.frames.has_animation("attack")):
 		animated_sprite.play("attack")
 
+	yield(get_tree().create_timer(0.1), "timeout")
 	yield(animated_sprite, "animation_finished")
+
+	if(!is_instance_valid(self)):
+		return
 
 	if(!is_instance_valid(target) || target == null):
 		state = GlobalEnums.UnitState.MOVING
 		return
 
-	target.receive_damage(attack_power)
-	attack_timer = 0
+	if(attack_type == GlobalEnums.UnitAttackType.RANGED):
+		if(projectile == null):
+			return
+
+		var projectile_instance = projectile.instance()
+		projectile_instance.global_position = global_position + projectile_offset
+
+		projectile_instance.set_target(target)
+		projectile_instance.set_damage(attack_power)
+		projectile_instance.set_group_list(group_list)
+
+		get_parent().add_child(projectile_instance)  # Adicione à cena primeiro
+		projectile_instance.set_velocity()  # Então configure a velocidade
+	elif(attack_type == GlobalEnums.UnitAttackType.MELEE):
+		target.receive_damage(attack_power)
+
 
 	if(target.health <= 0):
 		target = null
@@ -146,7 +173,11 @@ func attack():
 
 # Axuiliary functions
 func attack_animation_is_playing():
-	return animated_sprite.animation == "attack" && animated_sprite.frame < animated_sprite.frames.get_frame_count("attack") - 1
+	if(animated_sprite.animation == "attack"):
+		if(animated_sprite.frame < animated_sprite.frames.get_frame_count("attack") - 1):
+			return true
+
+	return false
 
 func set_state(new_state):
 	state = new_state
